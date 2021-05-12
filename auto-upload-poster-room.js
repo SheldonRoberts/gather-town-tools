58f9774233772cb2f06919417fd29d7e2b667343
Sheldon Roberts <sheldon5@ualberta.ca>
2021-05-12 17:44:32 -0600

    working version

--- auto-upload-poster-room.js
+++ auto-upload-poster-room.js
@@ -86,6 +86,23 @@ const writeMap = async (posterData) => {
 	let impassable = {}; // maps r,c to true if impassable
 	let posters = [];
 	let privateSpaces = [];
+	if (posterData == null) {
+		posterData = [...Array(24).keys()].map((i) => {
+			// actually fill in your own poster data here, this is just 24 identical ones
+			return {
+				posterImg:
+					"https://cdn.gather.town/v0/b/gather-town.appspot.com/o/assets%2F8ac167c6-e24b-4706-a19d-6079a3e30004?alt=media&token=4ff8d831-006c-4c9a-9ff0-740504b2bbd9",
+				posterPreview:
+					"https://cdn.gather.town/v0/b/gather-town.appspot.com/o/assets%2F9f48996d-9b50-480a-9ea6-46e98013204e?alt=media&token=c602f84d-8cf8-43a8-935a-b11a1030d8f0",
+				mapImg:
+					"https://cdn.gather.town/v0/b/gather-town.appspot.com/o/assets%2Ff34f6bf2-3bf9-4d95-9fe5-280e5da132a0?alt=media&token=9bc3f3c3-b691-4688-b633-2b0082e3f014",
+				mapImgActive:
+					"https://cdn.gather.town/v0/b/gather-town.appspot.com/o/assets%2Fbd842123-b038-4cf9-8e73-0fe335d56f0f?alt=media&token=86879c6d-2625-4379-8f23-6af0b22487d7",
+				numberImg:
+					"https://cdn.gather.town/v0/b/gather-town.appspot.com/o/assets%2F9ea396a7-924e-470b-ad4d-c40b1abe761a?alt=media&token=608596ac-9fd1-45ed-a8ae-5439495ddf39",
+			};
+		});
+	}
 
 	// arrange the posters in rows
 	posterData.forEach((poster, index) => {
@@ -163,11 +180,10 @@ const writeMap = async (posterData) => {
 // 	"/home/npfoss/Downloads/poster1.jpg",
 // 	"/home/npfoss/Downloads/poster2.jpg",
 // ]).then(console.log);
-const run = () => {
-	writeMap(posterData);
-	console.log('ran!')
-}
+
+//writeMap(posterData);
+
 
 module.exports ={
-        uploadFiles
+        uploadFiles, writeMap
     }
--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const multer = require('multer');
-const uploader = require('./auto-upload-poster-room')
+const uploader = require('./auto-upload-poster-room');
 const app = express();
 const PORT = process.env.PORT || 3000;
 var upload = multer({ dest: 'uploads/' })
+
 
 app.use(express.urlencoded({
   extended: true
@@ -11,8 +12,32 @@ app.use(express.urlencoded({
 
 app.post('/profile', upload.array('photos', 12), function (req, res, next) {
 
-  uploader.uploadFiles([req.files[0].path]).then(console.log);
-  res.json({ message: "done" });
+  let paths = []
+  for (const file of req.files) {
+    paths.push(file.path)
+  }
+
+  const generatePosters = (links) => {
+    console.log(links)
+    let posterData = [];
+    for (const file of req.files) {
+      console.log('loop ' + links[file.path])
+      posterData.push({
+      		posterImg: links[file.path],
+      		posterPreview: links[file.path],
+      		mapImg:
+      			"https://cdn.gather.town/v0/b/gather-town.appspot.com/o/assets%2Ff34f6bf2-3bf9-4d95-9fe5-280e5da132a0?alt=media&token=9bc3f3c3-b691-4688-b633-2b0082e3f014",
+      		mapImgActive:
+      			"https://cdn.gather.town/v0/b/gather-town.appspot.com/o/assets%2Fbd842123-b038-4cf9-8e73-0fe335d56f0f?alt=media&token=86879c6d-2625-4379-8f23-6af0b22487d7",
+      		numberImg:
+      			"https://cdn.gather.town/v0/b/gather-town.appspot.com/o/assets%2F9ea396a7-924e-470b-ad4d-c40b1abe761a?alt=media&token=608596ac-9fd1-45ed-a8ae-5439495ddf39",
+      	});
+    }
+    uploader.writeMap(posterData);
+  }
+
+  uploader.uploadFiles(paths).then(generatePosters);
+  res.json({ message: 'done' });
   // req.files is array of `photos` files
   // req.body will contain the text fields, if there were any
 })
@@ -23,16 +48,3 @@ app.listen(PORT, () => console.log("listening on port " + PORT))
 app.get('/',function(req,res) {
   res.sendFile(__dirname + '/index.html');
 });
-
-/*
-app.post('/submit-form', (req, res) => {
-  const username = req.body.username
-  const images = req.body.images
-  console.log(username)
-  console.log(images)
-  console.log(typeof images[0])
-  res.redirect('/submited')
-  res.end()
-})
-
-*/
