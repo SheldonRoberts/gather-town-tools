4e6fd401a3d63c53fdb3d28a2b1e9ad81faac1f9
Sheldon Roberts <sheldon5@ualberta.ca>
2021-05-12 14:07:19 -0600

    change

--- main.js
+++ main.js
@@ -47,18 +47,7 @@ app.post('/submit-form', (req, res) => {
     let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).array('multiple_images', 10);
 
     upload(req, res, function(err) {
-      if (req.fileValidationError) {
-          return res.send(req.fileValidationError);
-      }
-      else if (!req.file) {
-         return res.send('Please select an image to upload');
-      }
-      else if (err instanceof multer.MulterError) {
-         return res.send(err);
-      }
-      else if (err) {
-         return res.send(err);
-      }
+    
 
         let result = "You have uploaded these images: <hr />";
         const files = req.files;
