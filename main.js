d8f7249cf65c102458a1c12f7d07138ca43a984c
Sheldon Roberts <sheldon5@ualberta.ca>
2021-05-12 14:39:40 -0600

    a

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/images'));
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, 'uploads/');
+        cb(null, __dirname + '/images/uploads/');
     },
 
     // By default, multer removes file extensions so let's add them back
